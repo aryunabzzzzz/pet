e7825545467459c695e951ba15471062a66b50f9
aryuna <abanzanova09@gmail.com>
2024-07-17 13:18:01 +0700

    refactor: длинный метод
    длинный метод разделен на два метода

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -5,7 +5,6 @@ namespace App\Services;
 use App\Data\DTO\Order\CreateOrderDTO;
 use App\Events\OrderCreated;
 use App\Exceptions\CreateOrderException;
-use App\Http\Controllers\MailController;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
@@ -27,7 +26,16 @@ class OrderService
     public function create(CreateOrderDTO $DTO): Collection|JsonResponse
     {
         $order = Order::create($DTO->toArray());
-        $user = User::find($DTO->customerId);
+        $this->addFoodsIntoOrder($DTO->customerId, $order->id);
+
+        //отправка письма на почту через событие
+        event(new OrderCreated($order));
+        return Order::with('foods')->where('id',$order->id)->get();
+    }
+
+    public function addFoodsIntoOrder(int $userId, int $orderId)
+    {
+        $user = User::find($userId);
         //получение корзины пользователя
         $cart = $user->cart;
         if ($cart == null){
@@ -39,16 +47,11 @@ class OrderService
             throw new CreateOrderException('Корзина пуста');
         }
         //добавление продуктов в заказ
+        $order = Order::find($orderId);
         foreach ($foods as $food) {
             $price = $food->price * $food->pivot->quantity;
             $order->foods()->attach($food->id, ['quantity' => $food->pivot->quantity, 'price' => $price]);
         }
-
-        //отправка письма на почту через событие
-        event(new OrderCreated($order));
-
-        //добавить удаление(очищение) корзины
-        return Order::with('foods')->where('id',$order->id)->get();
     }
 
     public function delete(int $id): bool
