168e8525a81c2711350447bb662ad632994b68e2
aryuna <abanzanova09@gmail.com>
2024-10-13 01:26:00 +0700

    feat: add exportServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\FoodExport\ExportService;
-use App\Services\FoodExport\PdfExportService;
-use App\Services\FoodExport\XlsxExportService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
-
-//        $this->app->singleton(ExportService::class, XlsxExportService::class);
-        $this->app->singleton(ExportService::class, PdfExportService::class);
     }
 
     /**
